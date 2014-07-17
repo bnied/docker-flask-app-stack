1592b3cd1d1e6b9a93953b6a5649439e371b0bd4
Tavis Aitken <tavisto@tavisto.net>
2014-07-17 17:11:36 -0600

    Added build.sh file.

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+. config.sh
+while getopts "c" flag
+do
+    case "$flag" in
+        c) clean=1;echo "Running Clean";;
+    esac
+done
+
+if [[ $clean -gt 0 ]]; then
+    echo "Removing images before rebuilding"
+    $docker_cmd rmi $app_image 
+    $docker_cmd rmi $db_image 
+fi
+
+$docker_cmd build -t $app_image ./app
+$docker_cmd build -t $db_image ./db
